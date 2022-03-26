03085270d0ef96bd8b9feaa9ff2fbb2dbe366b70
ohkinozomu <nozomunoise@gmail.com>
2022-03-26 09:52:17 +0900

    Refactoring

---
+++ pkg/validator/validate.go
@@ -0,0 +1,116 @@
+package validator
+
+import (
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	"github.com/tidwall/gjson"
+	"sigs.k8s.io/yaml"
+)
+
+var (
+	//go:embed schemas/v3.0.json
+	schemaV30 []byte
+
+	//go:embed schemas/v3.1.json
+	schemaV31 []byte
+)
+
+type Version int
+
+const (
+	UNKNOWN Version = iota
+	V30
+	V31
+)
+
+func detectVersion(target string) (Version, error) {
+	log.Println("Detecting version...")
+	v := gjson.Get(target, "openapi")
+
+	if v.Str == "" {
+		return UNKNOWN, errors.New("invalid format")
+	}
+
+	log.Println("version: " + v.Str)
+
+	s := strings.Split(v.Str, ".")
+	if s[0] == "3" && s[1] == "0" {
+		return V30, nil
+	} else if s[0] == "3" && s[1] == "1" {
+		return V31, nil
+	}
+	return UNKNOWN, nil
+}
+
+func readFromFile(fileName string) (string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func yamlToJSON(target string) (string, error) {
+	j, err := yaml.YAMLToJSON([]byte(target))
+	if err != nil {
+		return "", err
+	}
+	return string(j), nil
+}
+
+func Validate(fileName string) error {
+	target, err := readFromFile(fileName)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	target, err = yamlToJSON(target)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	version, err := detectVersion(target)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	var schema string
+	switch version {
+	case V30:
+		schema = string(schemaV30)
+	case V31:
+		schema = string(schemaV31)
+	default:
+		log.Fatal("unknown version")
+	}
+
+	sch, err := jsonschema.CompileString("", schema)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	var v interface{}
+	if err := json.Unmarshal([]byte(target), &v); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = sch.Validate(v); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	log.Println("Validate OK!!")
+	return nil
+}
